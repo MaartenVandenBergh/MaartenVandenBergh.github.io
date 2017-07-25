445c5a219f7593dff818d30c23218ff28aca98c6
Maarten Van den Bergh <Maarten.Van.den.Bergh@gfi.be>
2017-07-25 13:35:47 +0200

    added scroll to top button + finalised sticky bar + implemented some fonts

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const HEADER_HEIGHT = document.getElementsByClassName("header")[0].clientHeight;
 const COLLAPSED_CLASSNAME = "collapsed";
 
-let busy = false;
+const scrollToTop = () => {
+    window.scrollTo(0,0);
+}
+
 const update = (time) => {
-    if(time > previousFrameTime + 250){
+    if(time > previousFrameTime + 100){
         const offset = window.pageYOffset;
         const header = document.getElementsByClassName("header")[0];
         const content = document.getElementsByClassName("content")[0];
         if(offset >= HEADER_HEIGHT - 66){
+            let marginTop = HEADER_HEIGHT;
             if(!header.classList.contains(COLLAPSED_CLASSNAME)){
                 header.classList.add(COLLAPSED_CLASSNAME);
-                content.style.marginTop = HEADER_HEIGHT + "px";
-                previousFrameTime = time;
+                marginTop += 49;
             }
+            if(previousOffset > offset){
+                if(!header.classList.contains("withNavigation")){
+                    header.classList.add("withNavigation");
+                }
+            }
+            else{
+                if(header.classList.contains("withNavigation")){
+                    header.classList.remove("withNavigation");
+                }
+            }
+            content.style.marginTop = marginTop + "px";
+            previousFrameTime = time;
         }
         else{
             if(header.classList.contains(COLLAPSED_CLASSNAME)){
@@ -21,18 +36,22 @@ const update = (time) => {
                 content.style.marginTop = '';
                 previousFrameTime = time;
             }
+            if(header.classList.contains("withNavigation")){
+                header.classList.remove("withNavigation");
+            }
         }
+        previousOffset = offset;
     }
 
 
     return false;
 };
+
+let previousOffset = 0;
 let previousFrameTime = 0;
 window.onscroll = (event) => {
     requestAnimationFrame(update);
 }
 
 console.log("js loaded");
-//window.addEventListener("scroll", adjustHeader);
-console.log("listeners added");
 
